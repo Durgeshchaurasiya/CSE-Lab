dd45baf186815845d84a90bffa816cc31c18905d
Shashank <113260615+shashankaz@users.noreply.github.com>
2023-01-28 12:17:54 +0530

    Create Lab 1 Q2.c

---
+++ Lab 1 Q2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>  
+int main()  
+{  
+      
+    int i, j, n, k;  
+    
+    printf ("Enter the Value of N : ");  
+    scanf("%d", &n);  
+
+    for (i=1;i<=n;i++)   
+    {  
+        for (j=i;j<n;j++)  
+        {  
+            printf(" ");   
+        }  
+        for (k=1;k<=i;k++)  
+        {  
+            if(i%2==0)
+            printf("$");
+            else
+            printf("*"); 
+        }  
+        printf ("\n");   
+    }  
+    
+    return 0;      
+}
