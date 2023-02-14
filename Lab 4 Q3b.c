625baaaf486a817e878d77cbf5b7cabf90db0740
Shashank <113260615+shashankaz@users.noreply.github.com>
2023-02-14 14:45:29 +0530

    Create Lab 4 Q3b.c

---
+++ Lab 4 Q3b.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int palindrome(int num) 
+{
+    int reversed = 0;
+    int original = num;
+
+    while (num > 0) 
+    {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    return original == reversed;
+}
+
+int main() 
+{
+    int num;
+
+    printf("Enter an Integer : ");
+    scanf("%d", &num);
+
+    if (palindrome(num)) 
+    {
+        printf("%d is a Palindrome\n", num);
+    } 
+    else 
+    {
+        printf("%d is Not a Palindrome\n", num);
+    }
+
+    return 0;
+}
